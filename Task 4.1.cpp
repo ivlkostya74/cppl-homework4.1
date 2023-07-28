d732f6988319e50ff7094501373e8029effe8bad
Konstantin Ivlev <ivlkostya@yandex.ru>
2023-07-28 20:48:02 +1000

    first commit

---
+++ Task 4.1.cpp
@@ -0,0 +1,24 @@
+﻿// CMakeProject1.cpp: определяет точку входа для приложения.
+//
+
+#include "List.h"
+#include "catch2/catch_test_macros.hpp"
+#include "catch2/catch_session.hpp"
+
+TEST_CASE("Testing List base functions", "[List]")
+{
+    List test;
+    CHECK(test.Empty() == true);
+    test.PushFront(23);
+    test.PushFront(12);
+    test.PushBack(76);
+    CHECK(test.Size() == 3);
+    test.Clear();
+    CHECK(test.Size() == 0);
+    CHECK(test.Empty() == true);
+}
+
+int main()
+{
+    return Catch::Session().run();
+}
